9f571812c761998e42951dc43f3c9a469826a4b3
Kevin Mera <kamcak@hotmail.es>
2021-12-07 03:25:50 -0500

    fetchs to db for city and venues selectors done

--- Proyecto final/Entregas/backend/src/main/java/config/ApplicationConfig.java
+++ Proyecto final/Entregas/backend/src/main/java/config/ApplicationConfig.java
@@ -18,6 +18,7 @@ public class ApplicationConfig extends Application {
         classes.add(services.CityServices.class);
         classes.add(services.PlanServices.class);
         classes.add(services.MembershipServices.class);
+        classes.add(services.VenueServices.class);
         return classes;
     }
 
--- Proyecto final/Entregas/backend/target/buddyTech/js/staff/createClient.js
+++ Proyecto final/Entregas/backend/target/buddyTech/js/staff/createClient.js
@@ -9,22 +9,37 @@ const weightTF = document.getElementById("weightTF");
 
 
 
-const getVenues = async ()=>{
+const getPlans = async ()=>{
 
     let plansNames = await fetch("http://localhost:8080/backend/api/ps/getactive");
     let names = await plansNames.json();
     console.log(names);
+    let html = `<option selected disabled selected hidden>Elegir Plan...</option>`;
+    
+    for(let i in names){
+        let incomingName = names[i];
+            html += `<option value="${i}">${incomingName}</option>`;        
+    }
+    planSelect.innerHTML = html;
+}
+
+getPlans();
+
+
+
+const getVenues = async ()=>{
+    let venuesNames = await fetch("http://localhost:8080/backend/api/venues/getvenues");
+    let venues = await venuesNames.json();
+    console.log(venues);
+    let html = `<option selected disabled selected hidden>Seleccionar Ciudad...</option>`;
+    for(let i in venues){
+        let incomingName = venues[i].name;
+            html += `<option value="${i}">${incomingName}</option>`;        
+    }
+    citySelect.innerHTML = html;
 }
 
 getVenues();
-
-
-
-const getPlans = async ()=>{
-
-
-
-}
 
 
 
